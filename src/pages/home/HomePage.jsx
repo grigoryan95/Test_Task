e1185c62e881391fc9e84f9bf56d4124ed93d50b
Andranik Grigoryan <grigoryanandranik95@gmail.com>
2022-07-28 10:51:43 +0430

    changes home, register page

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,8 +1,25 @@
 import {Redirect} from "react-router-dom";
+import {useAuth} from "../../hooks/use-auth";
+import {useDispatch} from "react-redux";
+import {removeUser} from "../../store/slices/userSlice";
 
 
 export const HomePage = () => {
+    const dispatch = useDispatch()
+    const {isAuth, email} = useAuth()
+    const handleRemove = () => {
+        dispatch(removeUser())
+    }
+
+    const test = isAuth ?
+            <div>
+                <p>home page</p>
+                <button onClick={handleRemove}>Log out from {email}</button>
+            </div>
+
+        : <Redirect to="/login" />
+
     return (
-        <Redirect to="/login" />
-    );
+        { test }
+    )
 };
--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,21 +1,31 @@
 import SignInSignUp from "../../component/sign_in_up/SignInSignUp";
 import {useDispatch} from "react-redux";
 import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+import {setUser} from "../../store/slices/userSlice";
+import {useHistory} from "react-router-dom";
 
 export const RegisterPage = () => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const {push} = useHistory()
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const auth = getAuth()
+        const auth = getAuth();
         const data = new FormData(event.currentTarget);
         const obj = {
             email: data.get('email'),
             password: data.get('password'),
         }
         createUserWithEmailAndPassword(auth, obj.email, obj.password)
-            .then(({user}) => console.log(user))
-            .catch()
+            .then(({user}) => {
+                dispatch(setUser({
+                    id: user.uid,
+                    email: user.email,
+                    token: user.accessToken
+                }))
+                push('/')
+            })
+            .catch(console.error)
     };
 
     return (
